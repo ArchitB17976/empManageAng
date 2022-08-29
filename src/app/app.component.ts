ee0b04e5b531442937da92bcc1934af814f3a831
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-29 13:49:02 +0800

    Editing main component to reflect intention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeServe } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'empManageAng';
+export class AppComponent implements OnInit
+{
+  ngOnInit() { this.getEmployees(); }
+
+  public employees: Employee[] = [];
+
+  constructor(private empService: EmployeeServe){}
+
+  public getEmployees(): void
+  {
+    this.empService.getEmployees().subscribe(
+      (response: Employee[]) => { this.employees = response; },
+      (error: HttpErrorResponse) => { alert(error.message); }
+    );
+  }
 }
